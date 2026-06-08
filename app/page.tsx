e1e3c0ade0fe072b71de15c61310504b3bf9957f
Denzel Chingodza <denzel.chingodza@icloud.com>
2026-06-08 20:43:26 +0200

    add formspree contact form endpoint

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
     const form = e.currentTarget;
     const data = new FormData(form);
     try {
-      const res = await fetch("https://formspree.io/f/YOUR_FORM_ID", {
+      const res = await fetch("https://formspree.io/f/xaqzozwr", {
         method: "POST",
         body: data,
         headers: { Accept: "application/json" },
